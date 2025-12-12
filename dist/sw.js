211bea7a176ddc135fd90ccbabbf7ad9459c35ea
Elson.Yeh <elson.yeh@tgc-taiwan.com.tw>
2025-12-12 13:19:21 +0800

    release

---
+++ dist/sw.js
@@ -0,0 +1,46 @@
+const CACHE_NAME = 'notelet-cache-v5';
+const ASSETS = [
+  './',
+  './index.html',
+  './sw.js',
+  './manifest.webmanifest',
+  './favicon.svg',
+  './asstes/icon-192.png',
+  './asstes/icon-512.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/jsonlint/1.6.0/jsonlint.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/js-beautify/1.14.9/beautify.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/js-beautify/1.14.9/beautify-css.min.js'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)).catch(() => {})
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(
+      keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k))
+    ))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  const { request } = event;
+  if (request.method !== 'GET') return;
+
+  event.respondWith(
+    caches.match(request).then((cached) => {
+      const fetchPromise = fetch(request).then((response) => {
+        if (response && response.status === 200 && response.type === 'basic') {
+          const cloned = response.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(request, cloned));
+        }
+        return response;
+      }).catch(() => cached || Promise.reject('offline'));
+
+      return cached || fetchPromise;
+    })
+  );
+});
